a72e4d782810db08cef60e06d5cf39eb45cfaf6a
takushikai <takushikai@gmail.com>
2025-09-19 02:04:38 +0900

    右に編集パネルを追加

--- ui_test/main.js
+++ ui_test/main.js
@@ -1,7 +1,7 @@
 const preview_canvas = document.getElementById("preview_canvas");
 
-// preview_canvas.width = 640;
-// preview_canvas.height = 480;
+preview_canvas.width = 640;
+preview_canvas.height = 480;
 
 const play_icon = document.getElementById("play_icon");
 const pause_icon = document.getElementById("pause_icon");
