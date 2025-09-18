b031f88164cf7328d25cb193fd7ed722c502b650
takushikai <takushikai@gmail.com>
2025-09-19 00:53:02 +0900

    first commit

---
+++ ui_test/main.js
@@ -0,0 +1,17 @@
+const preview_canvas = document.getElementById("preview_canvas");
+
+// preview_canvas.width = 640;
+// preview_canvas.height = 480;
+
+const play_icon = document.getElementById("play_icon");
+const pause_icon = document.getElementById("pause_icon");
+
+play_icon.addEventListener("click",()=>{
+    play_icon.hidden = true;
+    pause_icon.hidden = false;
+});
+
+pause_icon.addEventListener("click",()=>{
+    play_icon.hidden = false;
+    pause_icon.hidden = true;
+})
